4235d2d8111961ad8f37376b339c8f5a4b91d969
AlexUnguranu <a.ungureanu0099@gmail.com>
2023-03-08 16:27:57 +0200

    Added a custom exception

--- NFA To Regex/Exceptions/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace NFA_To_Regex.Exceptions
-{
-    internal class Class1
-    {
-
-    }
-}
---
+++ NFA To Regex/Exceptions/EmptyAutomateExceptions.cs
@@ -0,0 +1,14 @@
+﻿namespace NFA_To_Regex.Exceptions
+{
+    internal class EmptyAutomateExceptions : Exception
+    {
+        private const string DefaultMessage = "The automate is empty";
+
+        public EmptyAutomateExceptions() : base(DefaultMessage) { }
+
+        public EmptyAutomateExceptions(string message) : base(message)
+        {
+        }
+
+    }
+}
