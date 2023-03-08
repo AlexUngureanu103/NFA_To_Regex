d1e32c9ff03e237be2143be96989a94be9e9317f
AlexUnguranu <a.ungureanu0099@gmail.com>
2023-03-08 12:58:48 +0200

    Added a try cath for main program

---
+++ NFA To Regex/NFA To Regex/NFA.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFA_To_Regex
+{
+    internal class NFA
+    {
+        
+    }
+}
--- NFA To Regex/NFA To Regex/Program.cs
+++ NFA To Regex/NFA To Regex/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace NFA_To_Regex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+
+                }
+                catch (Exception ex)
+                {
+                    DisplayError(ex);
+                }
+                Pause();
+            }
+        }
+
+        private static void DisplayError(Exception ex)
+        {
+            ConsoleColor oldColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(ex);
+            Console.ForegroundColor = oldColor;
+        }
+
+        private static void Pause()
+        {
+            Console.WriteLine();
+            Console.Write("Press any key to continue...");
+            Console.ReadKey(true);
+            Console.WriteLine();
+        }
+
+    }
