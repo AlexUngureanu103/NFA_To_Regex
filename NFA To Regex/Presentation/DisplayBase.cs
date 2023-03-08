7d505cef48e6e1ad5c8947cca66094bc4ca2175e
AlexUnguranu <a.ungureanu0099@gmail.com>
2023-03-08 12:58:28 +0200

    Added a Presentation project

---
+++ NFA To Regex/Presentation/DisplayBase.cs
@@ -0,0 +1,21 @@
+﻿namespace NFA_To_Regex.Presentation
+{
+    internal class DisplayBase
+    {
+        public void DisplayLine(string message, ConsoleColor color)
+        {
+            ConsoleColor oldColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ForegroundColor = oldColor;
+        }
+
+        public void Display(string message, ConsoleColor color)
+        {
+            ConsoleColor oldColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.Write(message);
+            Console.ForegroundColor = oldColor;
+        }
+    }
+}
